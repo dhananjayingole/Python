6dfe4eb076b18e2aa39b61ac7121ecf49e16925c
Dhananjay Ingole <2023bcs116@sggs.ac.in>
2025-12-03 09:54:47 +0530

    Initial commit

---
+++ variables/variables.py
@@ -0,0 +1,20 @@
+# we will craetes some variables here
+name = "Dhananjay Ingole"
+age = 22
+is_student = True
+h1eight = 5.9
+# this aresome variables of different data types which can be written in this form only.
+# but we can't put num or special characters at the starting of the variable name.
+# also we can't use spaces in between the variable name.
+
+print(name)
+print(age)  
+print(is_student)
+print(h1eight)
+
+print(name, age, is_student, h1eight) # we can also print multiple variables in a single print statement by separating them with commas.
+
+# we verify the data types of the variables using the type() function.
+print(type(name))
+print(type(age))
+print(type(is_student))
